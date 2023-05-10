f579d7c489bba450be4067a0ac855a838a22c603
TaiwolfB <tailupbogdan@gmail.com>
2023-05-10 18:18:25 +0300

    adding input commands for run-server

--- client.cpp
+++ client.cpp
@@ -766,6 +766,7 @@ int do_attestation (sgx_enclave_id_t eid, config_t *config, char* deploymentFile
 		strcpy(msg5_encryption_request->deploymentFileLocation, deploymentFileLocation);
 		printf("FILE BEFORE = %s\n", msg5_encryption_request->deploymentFileLocation);
 		printf("Size before = %ld\n", sizeof(msg5_encryption_request));
+		printf("Size before = %ld\n", sizeof(msg5_encryption_request));
 		size_t msg5_sz = sizeof(msg5_encryption_request) / 2;
 		dividerWithText(stderr, "Copy/Paste Msg5 Below to SP");
 		// msgio->send(msg5_encryption_request, msg5_sz);
