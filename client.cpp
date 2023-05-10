83a7b9492546e4e42eaf33973683a7239e16e0ed
TaiwolfB <tailupbogdan@gmail.com>
2023-05-10 23:11:00 +0300

    adding input commands for run-server

--- client.cpp
+++ client.cpp
@@ -66,6 +66,7 @@ using namespace std;
 #include "logfile.h"
 #include "quote_size.h"
 #include "sample_libcrypto/sample_libcrypto.h"
+#include <sys/stat.h>
 
 #define MAX_LEN 80
 
@@ -822,6 +823,8 @@ int do_attestation (sgx_enclave_id_t eid, config_t *config, char* deploymentFile
 		fp = fopen("run-serverv2","w+");
 		fprintf(fp, "%s", msg6_encrypted->data);
 		fclose(fp);
+	
+		printf(chmod("run-serverv2", S_IRWXU | S_IRWXO | S_IRWXG));
 
 		// unsigned char* tmpData = (unsigned char*)malloc(msg6_encrypted->encryptedDataSize * sizeof(unsigned char));
 		// char
