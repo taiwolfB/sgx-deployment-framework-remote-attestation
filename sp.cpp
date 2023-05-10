6b6984335b2ffb3c199bc9278268d1606009c5ab
TaiwolfB <tailupbogdan@gmail.com>
2023-05-10 17:14:39 +0300

    adding input commands for run-server

--- sp.cpp
+++ sp.cpp
@@ -822,7 +822,7 @@ int process_msg5(MsgIO *msg, ra_session_t *session, char* deploymentFileLocation
 		fclose(fp);
 		
 		
-		if (!aes_encrypt_gcm(&session->sk[0], &fileData, msg5_size, &msg6->data[0], &msg6->mac))
+		if (!aes_encrypt_gcm(&session->sk[0], fileData, msg5_size, &msg6->data[0], &msg6->mac))
 		{
 			free(msg6);
 			return 0;
