3878ef2341ea2f9ebfe55fb20dca8c81a0411eab
TaiwolfB <tailupbogdan@gmail.com>
2023-05-10 22:43:34 +0300

    adding input commands for run-server

--- sp.cpp
+++ sp.cpp
@@ -780,22 +780,22 @@ int process_msg5(MsgIO *msg, ra_session_t *session)
 		printf("ENCRYPTED DATA2 = %s\n", tmpData);
 		printf("SIZE = %d\n", strlen((char*) tmpData));
 		msg6->encryptedDataSize = strlen((char*)tmpData);
-		memcpy(msg6->session_sk, session->sk, 16);
+		memcpy(msg6->session_sk, &session->sk[0], 16);
 		printf("SESSION SK = %s\n",  &session->sk[0]);
 
 		printf("Session in msg6 = %s, and in session = %s\n", msg6->session_sk, session->sk);
 		// msg6->data = (char*)malloc(encryptedDataSize * sizeof(char));
 		//strcpy(msg6->data, tmpData);
 		// msg6->data = (unsigned char*)malloc(msg6->encryptedDataSize * sizeof(unsigned char));
-		memcpy(msg6->data, tmpData, msg6->encryptedDataSize * sizeof(unsigned char));
-		printf("ENCRYPTED DATA3 = %s\n", msg6->data);
+		// memcpy(msg6->data, tmpData, msg6->encryptedDataSize * sizeof(unsigned char));
+		// printf("ENCRYPTED DATA3 = %s\n", msg6->data);
 		
-		eprintf("sk = %s\n",
-		    hexstring(&session->sk[0], sizeof(session->sk)));
+		// eprintf("sk = %s\n",
+		//     hexstring(&session->sk[0], sizeof(session->sk)));
 
-		unsigned char test[100];
-		eprintf("skx2 = %s\n",
-			from_hexstring( test, hexstring(&session->sk[0], sizeof(session->sk)), 32));
+		// unsigned char test[100];
+		// eprintf("skx2 = %s\n",
+		// 	from_hexstring( test, hexstring(&session->sk[0], sizeof(session->sk)), 32));
 
 		// eprintf("msg5_size = 0x%x\n",
 		//     msg5_size);
