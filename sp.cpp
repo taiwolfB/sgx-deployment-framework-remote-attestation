9e9c3ea45c2651b0e579e720ebfc1c35f6a08dec
TaiwolfB <tailupbogdan@gmail.com>
2023-05-10 21:28:58 +0300

    adding input commands for run-server

--- sp.cpp
+++ sp.cpp
@@ -812,7 +812,7 @@ int process_msg5(MsgIO *msg, ra_session_t *session)
 		printf("Size from ftell = %d\n Size after read = %d\n", fileSizeInBytes, fileDataSize);
 		fclose(fp);
 
-		if (!aes_encrypt_gcm(&session->sk[0], &fileData[0], msg5_size, &msg6->data[0], &msg6->mac))
+		if (!aes_encrypt_gcm(&session->sk[0], &fileData[0], fileDataSize, &msg6->data[0], &msg6->mac))
 		{
 			free(msg6);
 			return 0;
