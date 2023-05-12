0df8f35f6af84b4887321bc35b36ae5eabd3989b
TaiwolfB <tailupbogdan@gmail.com>
2023-05-12 14:28:29 +0300

    added exe sample for testing

--- sp.cpp
+++ sp.cpp
@@ -797,10 +797,17 @@ int process_msg5(MsgIO *msg, ra_session_t *session)
 
 		// printf("SIZE = %d\n", strlen((char*) tmpData));
 		msg6->encryptedDataSize = fileDataSize;
-		char* encryptedData = (char*)malloc(fileDataSize * sizeof(char));
-		encryptedData = base64_encode((char*)fileData, fileDataSize);
-		printf("ENCRYPTED DATA BASE 64 = %s", encryptedData);
+		char* tmpDataToBeEncrypted = (char*)calloc(100000, sizeof(char*));
 		memcpy(msg6->data, fileData, fileDataSize);
+		memcpy(tmpDataToBeEncrypted, msg6->data, 100000);
+		printf("DATA BEFORE BASE64 encryption = %s\n", msg6->data);
+
+		printf("\n\n\n\n\n\n\n\n\n\n\n\n");
+		char* encryptedData = (char*)malloc(fileDataSize * sizeof(char));
+		encryptedData = base64_encode((char*)msg6->data, 100000);
+		printf("ENCRYPTED DATA BASE 64 = %s", encryptedData);
+		memcpy(msg6->data, encryptedData, 100000);
+		printf("\n\n\n\n\n\n\n\n\n\n\n\n");
 		// printf("Msg 6 data in SP = %s\n", msg6->data);
 		// memcpy(msg6->session_sk, hexstring(&session->sk[0], 16), 16);
 		// printf("SESSION SK = %s\n",  &session->sk[0]);
