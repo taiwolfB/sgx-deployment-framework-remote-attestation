9a9b4d21f4a9d17c351e708b87230f2636b92b29
TaiwolfB <tailupbogdan@gmail.com>
2023-05-16 18:21:09 +0300

    adding libcrypto again

--- sp.cpp
+++ sp.cpp
@@ -763,7 +763,7 @@ int process_msg5(MsgIO *msg, ra_session_t *session)
 		}
 		
 		unsigned char read_data[100000];
-		size_t size_read = fread((unsigned char*)read_data, 1, stats.st_size, fp);
+		size_t size_read = fread((unsigned char*)read_data, stats.st_size, 1, fp);
 		if (verbose) {
 			printf("Size from stat = %d  Size after file_read = %d\n", stats.st_size, size_read);
 			printf("READ DATA = %s\n", read_data);
