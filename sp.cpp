cd2420b5e3aeac81e87b872fb441b19b0d5c5a91
TaiwolfB <tailupbogdan@gmail.com>
2023-05-16 17:54:55 +0300

    adding libcrypto again

--- sp.cpp
+++ sp.cpp
@@ -787,9 +787,10 @@ int process_msg5(MsgIO *msg, ra_session_t *session)
 			return 0;
 		}
 		printf("DATA ENCRYTPED = %s\n", msg6->data);
+		printf("ENCRYPTED DATA SIZE = %s\n", strlen(msg6->data));
 
 		unsigned char* decryptedData = (unsigned char*)malloc(msg6->encryptedDataSize * sizeof(unsigned char));
-		if (!aes_encrypt_gcm(msg6->session_sk, msg6->data, strlen((char*)msg6->data), &(decryptedData[0]), &macOut))
+		if (!aes_encrypt_gcm(msg6->session_sk, msg6->data, msg6->encryptedDataSize, &(decryptedData[0]), &macOut))
 		{
 			free(msg6);
 			return 0;
