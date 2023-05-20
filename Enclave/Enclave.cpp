fe1c2d3c6de3d3b875bfe54f0ebf61803aa273e6
TaiwolfB <tailupbogdan@gmail.com>
2023-05-21 00:38:35 +0300

    encryption

--- Enclave/Enclave.cpp
+++ Enclave/Enclave.cpp
@@ -26,7 +26,6 @@ in the License.
 #endif
 #include <sgx_tkey_exchange.h>
 #include <sgx_tcrypto.h>
-#include <iostream>
 
 static const sgx_ec256_public_t def_service_public_key = {
     {
@@ -181,8 +180,6 @@ sgx_status_t enclave_ra_get_key_hash(sgx_status_t *get_keys_ret,
 
 	*get_keys_ret= sgx_ra_get_keys(ctx, type, &k);
 	if ( *get_keys_ret != SGX_SUCCESS ) return *get_keys_ret;
-
-	std::cout << "KEY HERE"<< k << "\n";
 
 	/* Now generate a SHA hash */
 
