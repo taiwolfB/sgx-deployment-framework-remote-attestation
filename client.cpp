0db782e4c71a577279627c7c8d19d785ebaeb879
TaiwolfB <tailupbogdan@gmail.com>
2023-05-21 12:07:56 +0300

    added in enclave.cpp

--- client.cpp
+++ client.cpp
@@ -816,13 +816,13 @@ int do_attestation (sgx_enclave_id_t eid, config_t *config, char* deploymentFile
 		}
 
 		// printf("Encrpted data size received = %d\n", msg6_encrypted->encryptedDataSize);
-		printf("Encrypted data received = %s\n", msg6_encrypted->data);
-		printf("MSG6 SZ = %d\n", msg6_sz);
-		printf("SIZE RECEIVED  = %d\n", msg6_encrypted->encryptedDataSize);
-		printf("SK RECEIVED  = %s\n", msg6_encrypted->session_sk);
-		printf("SK SIZE RECEIVED = %d\n", msg6_encrypted->session_sk_size);
+		// printf("Encrypted data received = %s\n", msg6_encrypted->data);
+		// printf("MSG6 SZ = %d\n", msg6_sz);
+		// printf("SIZE RECEIVED  = %d\n", msg6_encrypted->encryptedDataSize);
+		// printf("SK RECEIVED  = %s\n", msg6_encrypted->session_sk);
+		// printf("SK SIZE RECEIVED = %d\n", msg6_encrypted->session_sk_size);
 		// sample_aes_gcm_128bit_tag_t macOut;
-		unsigned char decryptedData[100000];
+		// unsigned char decryptedData[100000];
 
 		// if (!aes_encrypt_gcm(msg6_encrypted->session_sk, &(msg6_encrypted->data[0]), msg6_encrypted->encryptedDataSize, &(decryptedData[0]), &macOut))
 		// {
@@ -830,21 +830,21 @@ int do_attestation (sgx_enclave_id_t eid, config_t *config, char* deploymentFile
 		// 	return 0;
 		// }
 
-		sgx_status_t ret = SGX_SUCCESS;
-		sgx_status_t sha_status, key_status;
-		// sgx_ec_key_128bit_t sk_key;/
-		sgx_sha256_hash_t skhash;
-		printf("RET BEFORE = %d\n", ret);
-		ret = enclave_ra_get_key_hash(eid, &sha_status, &key_status, ra_ctx, SGX_RA_KEY_SK, &skhash);
-		printf("SGX RET AFTER = %d\n", ret);
-		printf("HASH = %s\n", skhash);
+		// sgx_status_t ret = SGX_SUCCESS;
+		// sgx_status_t sha_status, key_status;
+		// // sgx_ec_key_128bit_t sk_key;/
+		// sgx_sha256_hash_t skhash;
+		// printf("RET BEFORE = %d\n", ret);
+		// ret = enclave_ra_get_key_hash(eid, &sha_status, &key_status, ra_ctx, SGX_RA_KEY_SK, &skhash);
+		// printf("SGX RET AFTER = %d\n", ret);
+		// printf("HASH = %s\n", skhash);
 
 		
-		// idea : ENCRYPT / DECRYPT with the hash itself
-		eprintf("SHA256(SK) = ");
-		print_hexstring(stderr, skhash, sizeof(skhash));
-		print_hexstring(fplog, skhash, sizeof(skhash));
-		eprintf("\n");
+		// // idea : ENCRYPT / DECRYPT with the hash itself
+		// eprintf("SHA256(SK) = ");
+		// print_hexstring(stderr, skhash, sizeof(skhash));
+		// print_hexstring(fplog, skhash, sizeof(skhash));
+		// eprintf("\n");
 
 
 		sgx_status_t get_signking_key_ret;
