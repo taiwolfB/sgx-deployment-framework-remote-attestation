24cd7da3d6e491386f1de03dda3b247fda4b424e
TaiwolfB <tailupbogdan@gmail.com>
2023-05-21 15:06:01 +0300

    added in enclave.cpp

--- sp.cpp
+++ sp.cpp
@@ -778,7 +778,7 @@ int process_msg5(MsgIO *msg, ra_session_t *session)
 		
 		printf("Starting AES encryptiong algorithm for the data\n");
 		unsigned char* encryptedData = (unsigned char*)malloc(msg6->fullDataToDecryptSize * sizeof(unsigned char));
-		if (!aes_encrypt_gcm(&session->sk[0], read_data, msg6->fullDataToDecryptSize,  &(encryptedData), &macOut))
+		if (!aes_encrypt_gcm(&session->sk[0], read_data, msg6->fullDataToDecryptSize,  &(encryptedData[0]), &macOut))
 		{
 			free(msg6);
 			return 0;
