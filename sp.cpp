196c9d58219de8063becbba7b89090ab657f4904
TaiwolfB <tailupbogdan@gmail.com>
2023-05-21 15:32:24 +0300

    added in enclave.cpp

--- sp.cpp
+++ sp.cpp
@@ -819,16 +819,16 @@ int process_msg5(MsgIO *msg, ra_session_t *session, ra_msg5_encryption_request_t
 		
 		// msgio->send_partial((void *) &msg6, sizeof(ra_msg6_encrypted_t));
 
-		
-		msgio->send_partial((void *) &msg6, sizeof(ra_msg6_encrypted_t));
-		fsend_msg_partial(fplog, (void *) &msg6, sizeof(ra_msg6_encrypted_t));
+		printf("SIZE OF MSG 6 = %d\n", sizeof(msg6));
+		// msgio->send_partial((void *) &msg6, sizeof(ra_msg6_encrypted_t));
+		// fsend_msg_partial(fplog, (void *) &msg6, sizeof(ra_msg6_encrypted_t));
 
-		msgio->send(&encryptedData, msg6->encryptedDataSize);
-		fsend_msg(fplog, &encryptedData, msg6->encryptedDataSize); 
+		// msgio->send(&encryptedData, msg6->encryptedDataSize);
+		// fsend_msg(fplog, &encryptedData, msg6->encryptedDataSize); 
 		// edivider();
         // msgio->send(&msg6->data, msg6->encryptedDataSize);
 		// msg6_size = sizeof(ra_msg6_encrypted_t);
-		// msgio->send(msg6, msg6_size);
+		msgio->send(msg6, msg6_size);
 		edivider();
 		free(msg6);
 	}
