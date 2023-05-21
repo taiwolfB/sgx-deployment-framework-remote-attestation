937ae34a24a08beb297b0e0321070bad36e91b2b
TaiwolfB <tailupbogdan@gmail.com>
2023-05-21 12:49:13 +0300

    added in enclave.cpp

--- sp.cpp
+++ sp.cpp
@@ -824,10 +824,11 @@ int process_msg5(MsgIO *msg, ra_session_t *session)
 
 		
 		// unsigned char encryptedDataTest[100000];
-		unsigned char encryptedData[100000];
+		// unsigned char encryptedData[100000];
 		sample_aes_gcm_128bit_tag_t macOut;
 		msg6->encryptedDataSize = stats.st_size;
 		// msg6->data = (unsigned char*)malloc(msg6->encryptedDataSize * sizeof(unsigned char));
+		printf("AICI\n");
 		if (!aes_encrypt_gcm(&session->sk[0], read_data, msg6->encryptedDataSize,  msg6->data, &macOut))
 		{
 			free(msg6);
