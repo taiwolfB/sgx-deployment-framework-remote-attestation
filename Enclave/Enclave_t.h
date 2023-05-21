c13da44be0d182118c057ac8474335116da8085d
TaiwolfB <tailupbogdan@gmail.com>
2023-05-21 11:00:28 +0300

    added in enclave.cpp

--- Enclave/Enclave_t.h
+++ Enclave/Enclave_t.h
@@ -27,7 +27,7 @@ sgx_status_t enclave_ra_close(sgx_ra_context_t ctx);
 sgx_status_t sgx_ra_get_ga(sgx_ra_context_t context, sgx_ec256_public_t* g_a);
 sgx_status_t sgx_ra_proc_msg2_trusted(sgx_ra_context_t context, const sgx_ra_msg2_t* p_msg2, const sgx_target_info_t* p_qe_target, sgx_report_t* p_report, sgx_quote_nonce_t* p_nonce);
 sgx_status_t sgx_ra_get_msg3_trusted(sgx_ra_context_t context, uint32_t quote_size, sgx_report_t* qe_report, sgx_ra_msg3_t* p_msg3, uint32_t msg3_size);
-
+sgx_status_t enclave_ra_get_signking_key(sgx_status_t* get_signking_key_ret, sgx_ra_context_t ctx, sgx_ra_key_type_t type, sgx_ra_key_128_t* key);
 
 #ifdef __cplusplus
 }
