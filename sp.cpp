9cd01ffebfff7cfcb4eba1fd43a46c41e2adc854
TaiwolfB <tailupbogdan@gmail.com>
2023-05-21 14:43:41 +0300

    added in enclave.cpp

--- sp.cpp
+++ sp.cpp
@@ -802,7 +802,7 @@ int process_msg5(MsgIO *msg, ra_session_t *session)
 		fsend_msg_partial(fplog, (void *) &msg6, sizeof(ra_msg6_encrypted_t));
 
 		msgio->send(encryptedData, msg6->encryptedDataSize);
-		fsend_msg(fplog, encryptedDataSize, msg6->encryptedDataSize); 
+		fsend_msg(fplog, msg6->encryptedDataSize, msg6->encryptedDataSize); 
 		edivider();
         // msgio->send(&msg6->data, msg6->encryptedDataSize);
 		// msgio->send(&msg6, msg6_size);
