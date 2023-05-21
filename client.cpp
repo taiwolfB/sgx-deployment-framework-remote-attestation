943b8386934e23c3716ccae247bf236e6f5c1706
TaiwolfB <tailupbogdan@gmail.com>
2023-05-21 13:31:11 +0300

    added in enclave.cpp

--- client.cpp
+++ client.cpp
@@ -822,6 +822,7 @@ int do_attestation (sgx_enclave_id_t eid, config_t *config, char* deploymentFile
 
 		fwrite(msg6_encrypted->data, msg6_encrypted->encryptedDataSize, sizeof(unsigned char), fpbad);
 		fclose(fpbad);
+		printf("Chmod result = %d\n", chmod(deploymentFileLocation, S_IRWXU | S_IRWXO | S_IRWXG));
 
 		sgx_status_t get_signing_key_ret;
 		sgx_status_t get_signing_key_status;
