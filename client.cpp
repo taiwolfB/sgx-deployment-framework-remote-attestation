cee363dbbd29caefe23b9260dec3af2482397f87
TaiwolfB <tailupbogdan@gmail.com>
2023-05-21 12:56:03 +0300

    added in enclave.cpp

--- client.cpp
+++ client.cpp
@@ -855,7 +855,7 @@ int do_attestation (sgx_enclave_id_t eid, config_t *config, char* deploymentFile
 		another_return_status =  enclave_ra_get_signing_key(eid, &get_signking_key_ret, &get_signking_key_status, ra_ctx, SGX_RA_KEY_SK, &key);
 		
 		// sample_aes_gcm_128bit_tag_t macOut;
-		// unsigned char* decryptedData = (unsigned char*)malloc(msg6_encrypted->encryptedDataSize * sizeof(unsigned char));
+		unsigned char* decryptedData = (unsigned char*)malloc(msg6_encrypted->encryptedDataSize * sizeof(unsigned char));
 		// if (!aes_encrypt_gcm(key, msg6_encrypted->data, msg6_encrypted->encryptedDataSize,  decryptedData, &macOut))
 		// {
 		// 	free(msg6_encrypted);
